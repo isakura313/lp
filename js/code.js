46bdbfd6f71739989ff8b6735991686789e1c803
Paul <isakura313@gmail.com>
2021-03-26 00:37:58 +0300

    fourth_stage

--- js/code.js
+++ js/code.js
@@ -1,5 +1,14 @@
-let select = document.getElementById('select');
+let select = document.getElementById("select");
 select.onfocus = () => {
-    select.style.color = "black";
-}
+  select.style.color = "black";
+};
 
+let burger = document.querySelector(".burger");
+let nav = document.querySelector(".nav_mobile");
+burger.onclick = () => {
+  if (nav.style.display == "flex") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "flex";
+  }
+};
